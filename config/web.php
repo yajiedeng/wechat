86bd5d6a22e9e376e7d2208e2a5854c6b8a44a8f
dengyajie <dengyajie@incar100.com>
2018-07-02 11:27:43 +0800

    测试回复消息

--- config/web.php
+++ config/web.php
@@ -15,7 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'fsdfds',
-//            'enableCsrfValidation' => false,
+            'enableCsrfValidation' => false,
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
