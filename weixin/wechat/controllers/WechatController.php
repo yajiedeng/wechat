04adadf8b359ee8a30ce33d7bd0a1505813a1ce7
dengyajie <dengyajie@incar100.com>
2018-07-03 15:46:35 +0800

    增加模块

--- weixin/wechat/controllers/WechatController.php
+++ weixin/wechat/controllers/WechatController.php
@@ -29,7 +29,7 @@ class WechatController extends WeixinController
             $keywords = $messageObj->Content;//接收关键字
             $message  = new MessageController();
             $content = "您刚才讲 ： ".$keywords;
-            $message->actionResponseText();
+            $message->actionResponseText($content);
         }
     }
 
